0bf89905da6b7064fb2e879c9fbfb0bcebb69ba7
GutterLab <43664214+GutterLab@users.noreply.github.com>
2019-01-18 15:24:34 -0800

    Copied the wrong file into the repo, it was outdated.

--- io_export_animationseparator.py
+++ io_export_animationseparator.py
@@ -14,9 +14,12 @@ import os
 
 
 def main(context):
-#    print("test")
     startStates = []
-
+    blend_file_path = bpy.data.filepath
+    directory = os.path.dirname(blend_file_path)
+    
+#    target_file = os.path.join(directory, os.path.splitext(context.blend_data.filepath)[0] + '.fbx')
+#    bpy.ops.export_scene.fbx(filepath = target_file, check_existing = True, object_types = {'MESH'})
     #record original state
     for i in range(0, len(bpy.data.actions)):
         print(str(i) + " " + str(bpy.data.actions[i]) + " " + str(bpy.data.actions[i].use_fake_user))
@@ -25,15 +28,17 @@ def main(context):
     
     count = 0
     for i in range(0, len(bpy.data.actions)):
-        if startStates[i]:
+        if bpy.data.actions[i].name == 'ArmatureAction':
+            print('skipping')
+
+        elif startStates[i]:
             bpy.data.actions[i].use_fake_user = True
             #export here
-            blend_file_path = bpy.data.filepath
-            directory = os.path.dirname(blend_file_path)
-            #target_file = os.path.join(directory, bpy.path.basename(context.blend_data.filepath) + '_' + bpy.data.actions[i].name + '.fbx')
+            context.scene.frame_end = bpy.data.actions[i].frame_range[1]
+
             target_file = os.path.join(directory, os.path.splitext(context.blend_data.filepath)[0] + '_' + bpy.data.actions[i].name + '.fbx')
-            
-            bpy.ops.export_scene.fbx(filepath = target_file, check_existing = True, object_types = {'ARMATURE'}, bake_anim_use_nla_strips = False, bake_anim_use_all_actions = False, bake_anim_force_startend_keyring = False)
+            print(str(bpy.data.actions[i].name))
+            bpy.ops.export_scene.fbx(filepath = target_file, check_existing = False, object_types = {'ARMATURE', 'MESH'}, bake_anim_use_nla_strips=False, bake_anim_use_all_actions=False, bake_anim_force_startend_keying=False)
             bpy.data.actions[i].use_fake_user = False
         
     #revert to what it was
